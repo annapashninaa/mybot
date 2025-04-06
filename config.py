196a6d5475fb7cb9f35cc8533c0216191534254e
annapashninaa <pashninan100@icloud.com>
2025-04-06 20:15:14 +0500

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-API_KEY = os.getenv("KINOSEARCH_API_KEY")
+TELEGRAM_TOKEN = "7930707459:AAFZNX7mOmja447jbv-sARsANzRC_Jt3-Sw"
+KINOPOISK_API_KEY = "VAA2NSE-FRM4Z8G-JYP6R1Q-K754NWZ"
 DATABASE_NAME = "movie_search.db"
